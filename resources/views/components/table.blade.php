e8cf7194cf8b458a631692d00a0e00a874654135
Antonio <antonio.perez.carmona.2005@gmail.com>
2025-01-23 13:29:22 +0100

    agregando ->odd

--- resources/views/components/table.blade.php
+++ resources/views/components/table.blade.php
@@ -10,19 +10,15 @@
             </tr>
         </thead>
         <tbody>
-            @for ($i = 0; $i < count($tableData['data']); $i++)
-                @if ($i % 2 == 0)
-                    <tr class="bg-gray-100 border-b dark:bg-gray-800 dark:border-gray-700">
-                @else
-                    <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-                @endif
-                    @foreach ($tableData['data'][$i] as $cell)
+            @foreach ($tableData['data'] as $row)
+                <tr class="{{ $loop->odd ? 'bg-gray-100' : 'bg-white' }} border-b dark:bg-gray-800 dark:border-gray-700">
+                    @foreach ($row as $cell)
                         <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
                             {{ $cell }}
                         </th>
                     @endforeach
                 </tr>
-            @endfor
+            @endforeach
         </tbody>
     </table>
 </div>
